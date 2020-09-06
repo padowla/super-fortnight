210902a1065b23591a957ed332a4605e84473bb5
lilpilgit <62257411+lilpilgit@users.noreply.github.com>
2020-09-06 19:40:29 +0200

    Update readRules.php

--- readRules.php
+++ readRules.php
@@ -1,5 +1,6 @@
 #!/usr/local/bin/php-cgi -f
 <?php
+
 /*   
     The /usr/local/etc/php.ini file contains the include_path entry
     which specifies a list of directories, separated by ":", where the functions require(),
@@ -11,12 +12,14 @@
     Inside / etc / inc there are the php filter.inc and config.inc files necessary for the function
     of the script.
 */
+
 #require_once("globals.inc");
 require_once("filter.inc");
 #require_once("util.inc");
 require_once("config.inc"); /* config.inc ==> config.lib.inc */
 
 global $config;
+$result = array("success" => false); #by default there is an error...pessimistic approach
 
 if (count($argv) == 1){
 	echo "Pass the interface (ex: wan) please!\n";
@@ -39,33 +42,35 @@ $interface = $argv[1];
  *   $config      - array containing all configuration variables
 
  ******/ 
+
 $config = parse_config(true);
-$retval = 42;
 
 function readJSONRules($config, $interface) {
 	
 	$rules = array();
+	global $result;
 
 	for($i=0; $i < count($config[filter][rule]); $i++){
 
 		$rule = $config[filter][rule][$i]; #save single rule to analyze in a local variable
 		
 		if( strcmp($rule["interface"], $interface) === 0){
-			
 			$rules[] = $rule; #add rule to array
 		}
 	}
+	
+	# modify result's array
+	$result["success"] = true;
+	
+	$to_return = array("result" => $result, "rules" => $rules);
 
-	global $retval;
-        $retval	= 0; #all it's fine
-	return json_encode($rules); #return JSON of rules for specified interface
+	return json_encode($to_return); #return JSON with result = true/false & rules for specified interface
 
 }
 
 echo readJSONRules($config, $interface);
 
-exit($retval);
+exit();
 
 
 ?>
-
